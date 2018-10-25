1ce512a1d0c853a09b41bd91b93f5559b5b77a1e
Paul Hatcherian <1835615+PaulHatch@users.noreply.github.com>
2018-10-25 05:58:42 -0400

    Testing failing test

--- init.sh
+++ init.sh
@@ -7,10 +7,8 @@ case "$1" in
 	shift
 	apk add py-pip
 	pip install docker-compose==1.22.0
-	docker-compose --version
-	docker pull $IMAGE_TAG
-	IMAGE=${1} docker-compose -f docker-compose.${2}.yml run test
-	docker cp $(docker ps -f label=test -a -q):/sln/results.xml .
+	IMAGE_TAG=${1} docker-compose -f docker-compose.${2}.yml run test
+	docker cp $(docker ps -f label=test -a -l -q):/sln/results.xml .
 	;;
 	*)
   echo Invalid command, use '--build', '--test', '--integration' or '--publish'
