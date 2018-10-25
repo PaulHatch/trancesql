b9aa3a92c4ed200c63e3da1e3f10b74046bb2c7f
Paul Hatcherian <1835615+PaulHatch@users.noreply.github.com>
2018-10-24 22:50:29 -0400

    Adding integration tests

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This file is run directly on the build host
+
+case "$1" in
+  "--integration")
+	shift
+	apk add py-pip
+	pip install docker-compose==1.22.0
+	docker-compose --version
+	docker pull $IMAGE_TAG
+	IMAGE=${1} docker-compose -f docker-compose.${2}.yml run test
+	docker cp $(docker ps -f label=test -a -q):/sln/results.xml .
+	;;
+esac
