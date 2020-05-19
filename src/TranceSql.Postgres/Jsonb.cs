5750c1940d04d76a0922d11c44bea92ee7c7c92d
Paul Hatcherian <1835615+PaulHatch@users.noreply.github.com>
2020-05-18 23:10:04 -0400

    Add initial jsonb function helper.

---
+++ src/TranceSql.Postgres/Jsonb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TranceSql.Postgres
+{
+    /// <summary>
+    /// Defines helper methods for creating Postgres jsonb function declarations.
+    /// </summary>
+    public static class Jsonb
+    {
+        /// <summary>
+        /// Creates a jsonb_set function which update the target at the
+        /// specified path with the new value provided, returning the
+        /// updated target as the result.
+        /// </summary>
+        /// <param name="target">Value to update</param>
+        /// <param name="path">Path to set.</param>
+        /// <param name="newValue">New value to replace at path.</param>
+        /// <param name="createMissing">Boolean value indicating whether
+        /// the value should be created if missing.</param>
+        /// <returns>Function call with specified parameters.</returns>
+        public static Function JsonbSet(ISqlElement target, JsonPath path, ISqlElement newValue, ISqlElement createMissing)
+            => new Function("jsonb_set", target, Constant.Unsafe($"{{{path}}}"), newValue, createMissing);
+
+        /// <summary>
+        /// Creates a jsonb_set function which update the target at the
+        /// specified path with the new value provided, returning the
+        /// updated target as the result.
+        /// </summary>
+        /// <param name="target">Value to update</param>
+        /// <param name="path">Path to set.</param>
+        /// <param name="newValue">New value to replace at path.</param>
+        /// <param name="createMissing">Boolean value indicating whether
+        /// the value should be created if missing.</param>
+        /// <returns>Function call with specified parameters.</returns>
+        public static Function JsonbSet(ISqlElement target, JsonPath path, ISqlElement newValue, bool createMissing)
+            => JsonbSet(target, String.Join(",", path), newValue, new Constant(createMissing));
+    }
+}
